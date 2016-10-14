ea34881e4209b0d4b20e61625f02badd7061608b
Devorah Langsam <devorah@langsam.org>
2016-10-14 17:47:29 -0400

    Fixed filter js

--- filter.js
+++ filter.js
@@ -1,18 +1,41 @@
 var fs = require('fs') 
+const path = require('path');
 
-function hasExtension(fileName, ext){
+function hasExtension(fileName, index, arr){
+	var fileExt = path.extname(fileName);
 
-	return (fileName.includes(ext));
-};
+	var result = fileExt.includes(ext)
 
 
+	if(result){
+		console.log(fileName);
+	}
+	//return fileName.length > 10;
+
+}
 //Asynchronously reads the entire contents of a file. Example:
+var ext = process.argv[3];
 
 fs.readdir(process.argv[2], function (err, list) {
   if (err) throw err;
-  list.filter(hasExtension, process.argv[3])
-  list.forEach(function(file){
-  	console.log(file);
-  });
+  list.filter(hasExtension, ext)
+
 	
 });
+
+
+     // var fs = require('fs')  
+     // var path = require('path')  
+       
+     // var folder = process.argv[2]  
+     // var ext = '.' + process.argv[3]  
+       
+     // fs.readdir(folder, function (err, files) {  
+     //   if (err) return console.error(err)  
+     //   files.forEach(function (file) {  
+     //     if (path.extname(file) === ext) {  
+     //       console.log(file)  
+     //     }  
+     //   })  
+     // })  
+   
